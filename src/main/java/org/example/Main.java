859e312d761f31fe550ad80308d9e133b91004f2
ykvelandia <velandia416@gmail.com>
2023-11-16 17:42:36 -0500

    Agregue un metodo saludoEstudiante

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!, I am  creating my repository");
+        System.out.println("Ya he iniciado mi repositorio local");
+
+        Main.saludoEstudiante("Karina");
+    }
+    private static void saludoEstudiante (String nombre){
+        System.out.println( "Hola soy el estudiante: " + nombre);
+    }
+
+}
