ca13b322af6a08f83dfa87b46124451f0c9be2c5
ykvelandia <velandia416@gmail.com>
2023-11-20 17:02:10 -0500

    Add potencia method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,16 @@ public class Main {
     }
     private static void saludoEstudiante (String nombre){
         System.out.println( "Hola soy el estudiante: " + nombre);
+
+    }
+
+    private static double   potencia(double base,double exponente){
+        double potencia = 1;
+        for (int i  = 0;i <exponente; i++){
+            potencia *= base;
+        }
+        return potencia;
+
     }
 
 }
