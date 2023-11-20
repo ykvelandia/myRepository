d1f91022969447e82df75caf3f4843ed15410e94
ykvelandia <velandia416@gmail.com>
2023-11-20 17:51:11 -0500

    Call potencia method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        double number1 = 2;
+        double  number2 = 2;
         System.out.println("Hello world!, I am  creating my repository");
         System.out.println("Ya he iniciado mi repositorio local");
+        System.out.println("La potencia es: " +potencia(number1,number2));
 
         Main.saludoEstudiante("Karina");
     }
